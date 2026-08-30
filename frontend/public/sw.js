833e972bdb02ee2cc896eca3e8224742fa664fe3
Sello Motloung <livingodfellowship@gmail.com>
2026-08-30 21:48:06 +0200

    Add installable CLGF CMS PWA

---
+++ frontend/public/sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'clgf-cms-v1';
+
+const APP_SHELL = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/app-icon-192x192.png',
+  '/app-icon-512x512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  const url = new URL(event.request.url);
+
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then((cached) =>
+        cached || caches.match('/index.html')
+      )
+    )
+  );
+});
